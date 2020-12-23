1fc4801f1f0a9c42215fc3234e56fd422ff54520
Nataliia Radina <nataliia@begame.nl>
2020-12-23 18:51:50 +0100

    Add icons, variables, genneral styles

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,39 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+html,
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  font-family: $base-font-family;
+  font-size: 14px;
+  color: #2c3e50;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+}
+._container {
+  max-width: 319px;
+  margin: 0 auto;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+._primary-link {
+  color: $blue;
+  font-weight: 700;
+  text-decoration: none;
+}
+._secondary-link {
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 15px;
+  text-decoration: underline;
+  color: $black;
 }
 </style>
